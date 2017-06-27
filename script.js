820029e002e256b29c41302b4ecc612367f5014e
Edgar Juarez <ejjt.686@gmail.com>
2017-06-26 17:28:12 -0700

    User can now enter minutes in the form

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const buttons = document.querySelectorAll('[data-time]');
 function timer(seconds) {
     // clear any existing timers
     clearInterval(countdown);
-    
+
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
@@ -46,3 +46,10 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+});
